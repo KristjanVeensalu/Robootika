31d609d661702be7b65a0c7ceaf7257d48a506b1
Guren <Kristjan.Veensalu@gmail.com>
2020-05-07 03:40:10 +0300

    Added automatic spellcheck, refactor work

--- functions.py
+++ functions.py
@@ -4,16 +4,21 @@ from tkinter import filedialog
 import subprocess
 import sys
 
-root = tk.Tk()
-root.withdraw()
 
-file_path = filedialog.askdirectory()
-directory = file_path
 
-reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
-installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+def chooseFolder():
+    root = tk.Tk()
+    root.withdraw()
+
+    file_path = filedialog.askdirectory()
+    directory = file_path
+    return directory
+
 
 def install(package):
+    reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+    installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
     if package in installed_packages:
         pass
     else:
@@ -22,7 +27,7 @@ def install(package):
         #subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
-def parseFolder():
+def parseFolder(directory):
     fileList=[]
     for filename in os.listdir(directory):
         if filename.endswith(".txt") or filename.endswith(".doc"):
@@ -31,3 +36,9 @@ def parseFolder():
         else:
             continue
     return fileList
+
+def printLine():
+    print("\n")
+    for x in range(0,40):
+        print("-", end = " ")
+    print("\n")
--- main.py
+++ main.py
@@ -1,102 +1,117 @@
 import functions as func
-func.install("nltk")
-import nltk
-#nltk.download()
 func.install("pyenchant")
 import enchant
+from enchant.checker import SpellChecker
+from enchant.tokenize import EmailFilter, URLFilter
+chkr = SpellChecker("en_US",filters=[EmailFilter,URLFilter])
 d=enchant.Dict("en_US")
-selectedFiles = func.parseFolder()
-#word_list = words.words()
-#print(len(word_list))
-#for x in selectedFiles:
-    #f=open(x, encoding='utf-8')
-    #print(f.read())
-print(selectedFiles)
 
-for fileItself in selectedFiles:
-    print(fileItself)
-    f=open(fileItself, encoding='utf-8')
-    textPhase = f.read()
-    splitList = textPhase.split(" ")
-    f.close()
-    phraseIndex = 0
-    listIndexProvider = []
+selectedFolder = func.chooseFolder()
+selectedFiles = func.parseFolder(selectedFolder)
 
-    for x in splitList:
-            if "\n" in x:
-                splitList[splitList.index(x)] = x.replace("\n","")
+for x in selectedFiles:
+    print("File found: "+x)
+filesFoundSum = len(selectedFiles)
+print("Files found: " + str(filesFoundSum))
 
-    for x in splitList:
-            if "\t" in x:
-                splitList[splitList.index(x)] = x.replace("\t","")
+try:
+    for enchantFiles in selectedFiles:
+        func.printLine()
+        f=open(enchantFiles, encoding='utf-8')
+        textPhase = f.read()
+        chkr.set_text(textPhase)
+        for err in chkr:
+            print("Word: ('" + err.word + "') In: " + enchantFiles)
+            failedWord = d.suggest(err.word)
+            print("Here are a few suggestions: ", end =" ")
+            print(failedWord)
 
-    for x in splitList:
-            if ":" in x:
-                splitList[splitList.index(x)] = x.replace(":","")
+except:
+    print("Automatic failed, switching to manual...")
+    for fileItself in selectedFiles:
+        print(fileItself)
+        f=open(fileItself, encoding='utf-8')
+        textPhase = f.read()
+        splitList = textPhase.split(" ")
+        f.close()
+        phraseIndex = 0
+        listIndexProvider = []
 
-    for x in splitList:
-            if ";" in x:
-                splitList[splitList.index(x)] = x.replace(";","")
-
-    for z in range(0,10):
         for x in splitList:
-            if x.endswith("."):
-                if x == "e.g." or x == "etc." or x == "eg." or x =="e.g":
-                    splitList[splitList.index(x)] = x.replace(".","")
-                else:
-                    phraseIndex = phraseIndex + 1
-                    listIndexProvider.append(splitList.index(x))
-                    splitList[splitList.index(x)] = x.replace(".","")
-            if x.endswith(","):
-                splitList[splitList.index(x)] = x.replace(",","")
+                if "\n" in x:
+                    splitList[splitList.index(x)] = x.replace("\n","")
 
-    for x in splitList:
-        if x.endswith('"') or x.startswith('"'):
-            splitList[splitList.index(x)] = x.replace('"',"")
+        for x in splitList:
+                if "\t" in x:
+                    splitList[splitList.index(x)] = x.replace("\t","")
 
-    modifiedIndexProvider = []
+        for x in splitList:
+                if ":" in x:
+                    splitList[splitList.index(x)] = x.replace(":","")
 
-    for x in listIndexProvider:
-        x = x+1
-        modifiedIndexProvider.append(x)
+        for x in splitList:
+                if ";" in x:
+                    splitList[splitList.index(x)] = x.replace(";","")
 
-    if len(modifiedIndexProvider)>1:
-        modifiedIndexProvider.remove(modifiedIndexProvider[len(modifiedIndexProvider)-1])
-
-
-    for x in modifiedIndexProvider:
-        if len(modifiedIndexProvider)>1:
-            newPhraseWord = splitList[x]
-            splitList[x] = newPhraseWord + "NEW"
-
-
-    faultyWords = []
-
-    for x in splitList:
-        if x == splitList[0] and x.isupper():
-            d.check(x)
-            if d.check(x) == False:
-                faultyWords.append(x)
-        else:
-            if x == "eg":
-                pass
-            else:
-                if not x:
-                    splitList.remove(x)
-                else:
-                    if x[0].isupper() and "NEW" not in x:
-                        pass
+        for z in range(0,10):
+            for x in splitList:
+                if x.endswith("."):
+                    if x == "e.g." or x == "etc." or x == "eg." or x =="e.g":
+                        splitList[splitList.index(x)] = x.replace(".","")
                     else:
-                        if "NEW" not in x:
-                            if d.check(x) == False:
-                                faultyWords.append(x)
+                        phraseIndex = phraseIndex + 1
+                        listIndexProvider.append(splitList.index(x))
+                        splitList[splitList.index(x)] = x.replace(".","")
+                if x.endswith(","):
+                    splitList[splitList.index(x)] = x.replace(",","")
+
+        for x in splitList:
+            if x.endswith('"') or x.startswith('"'):
+                splitList[splitList.index(x)] = x.replace('"',"")
+
+        modifiedIndexProvider = []
+
+        for x in listIndexProvider:
+            x = x+1
+            modifiedIndexProvider.append(x)
+
+        if len(modifiedIndexProvider)>1:
+            modifiedIndexProvider.remove(modifiedIndexProvider[len(modifiedIndexProvider)-1])
+
+
+        for x in modifiedIndexProvider:
+            if len(modifiedIndexProvider)>1:
+                newPhraseWord = splitList[x]
+                splitList[x] = newPhraseWord + "NEW"
+
+
+        faultyWords = []
+
+        for x in splitList:
+            if x == splitList[0] and x.isupper():
+                d.check(x)
+                if d.check(x) == False:
+                    faultyWords.append(x)
+            else:
+                if x == "eg":
+                    pass
+                else:
+                    if not x:
+                        splitList.remove(x)
+                    else:
+                        if x[0].isupper() and "NEW" not in x:
+                            pass
                         else:
-                            holder = x.replace("NEW","")
-                            if d.check(holder) == False:
-                                faultyWords.append(holder)
+                            if "NEW" not in x:
+                                if d.check(x) == False:
+                                    faultyWords.append(x)
+                            else:
+                                holder = x.replace("NEW","")
+                                if d.check(holder) == False:
+                                    faultyWords.append(holder)
 
-    wordPosition = 0
+        wordPosition = 0
 
-    for x in faultyWords:
-        wordPosition = splitList.index(x)
-        print("Word: (" + x + ") In> " + fileItself) 
+        for x in faultyWords:
+            wordPosition = splitList.index(x)
+            print("Word: (" + x + ") In> " + fileItself) 
